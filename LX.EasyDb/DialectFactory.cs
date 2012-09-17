6c97f2ecf3ec043eec100264e928466105511474
longshine <longxianghe@gmail.com>
2012-09-17 17:32:34 +0800

    add comment

--- LX.EasyDb/DialectFactory.cs
+++ LX.EasyDb/DialectFactory.cs
@@ -1,4 +1,16 @@
-﻿using System;
+﻿//
+// LX.EasyDb.DialectFactory.cs
+//
+// Authors:
+//	Longshine He <longshinehe@users.sourceforge.net>
+//
+// Copyright (c) 2012 Longshine He
+//
+// This code is distributed in the hope that it will be useful,
+// but WITHOUT WARRANTY OF ANY KIND.
+//
+
+using System;
 
 namespace LX.EasyDb
 {
