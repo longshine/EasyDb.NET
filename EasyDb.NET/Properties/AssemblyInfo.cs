fb1b5b8790a3955490d4d07625b10ac8ed0973ac
longshine <longxianghe@gmail.com>
2013-06-09 12:24:43 +0800

    update version

--- EasyDb.NET/Properties/AssemblyInfo.cs
+++ EasyDb.NET/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("EasyDb.NET")]
-[assembly: AssemblyCopyright("Copyright © 2009 - 2012 Longshine")]
+[assembly: AssemblyCopyright("Copyright © 2009 - 2013 Longshine")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -34,5 +34,5 @@ using System.Runtime.InteropServices;
 // 可以指定所有这些值，也可以使用“内部版本号”和“修订号”的默认值，
 // 方法是按如下所示使用“*”:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.82")]
-[assembly: AssemblyFileVersion("0.82")]
+[assembly: AssemblyVersion("0.84")]
+[assembly: AssemblyFileVersion("0.84")]
