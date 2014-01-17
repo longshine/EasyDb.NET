3b8b8990cbb7e7a6271d566e6f968598665a1106
longshine <longxianghe@gmail.com>
2014-01-17 16:50:45 +0800

    year to 2014

--- EasyDb.NET/Properties/AssemblyInfo.cs
+++ EasyDb.NET/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("EasyDb.NET")]
-[assembly: AssemblyCopyright("Copyright © 2009 - 2013 Longshine")]
+[assembly: AssemblyCopyright("Copyright © 2009 - 2014 Longshine")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
